98d9af35797a2aec0921202d0fad5607aa7f16b7
bangerius <sebastian.bangerius@gozinto.se>
2015-05-25 02:34:56 +0200

    Rättade till impullssvarsfunktionen
    hade glömt en kvadrat

--- impulssvar.m
+++ impulssvar.m
@@ -3,8 +3,8 @@ c=100;
 k=1000;
 m=70;
 
-t = linspace(0,5,1000);
-y=(1./(sqrt((4.*m.*k-c.*c)./4)).*exp(-c/(2.*m).*t).*sin(sqrt((4.*m.*k-c.*c)./(4.*m)).*t));
+t = linspace(0,10,1000);
+y=(1./(sqrt((4.*m.*k-c.*c)./4)).*exp(-c/(2.*m).*t).*sin(sqrt((4.*m.*k-c.*c)./(4.*m.^2)).*t));
 
 figure
 plot(y)
