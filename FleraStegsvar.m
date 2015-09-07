53617189a9c72995fbb9be8b8a8e12ad5ff2de02
Villpower <v.rydfalk@gmail.com>
2015-09-07 15:51:14 +0200

    Rätt bild och lite text

--- FleraStegsvar.m
+++ FleraStegsvar.m
@@ -7,18 +7,25 @@ k = 2000;
 c = 100;
 m = 70;
 
+w = sqrt(-(c/(2*m))^2 + (k/m));
+a = c/(2*m);
+
 t = linspace(0,10,1000);
 
-y = (1./(sqrt((4.*m.*k-c.*c)./4)).*exp(-c/(2.*m).*t).*sin(sqrt((4.*m.*k-c.*c)./(4.*m.^2)).*t));
+y = 1./(w*m).*(1 - cos(w.*t).*exp(-a.*t) -a./w.*sin(w.*t).*exp(-a.*t))./(w + ((a.^2)./w));
 hFig = figure(1);
 set(hFig, 'Position', [0,0,700,400]);
 
 c = 10;
-u = (1./(sqrt((4.*m.*k-c.*c)./4)).*exp(-c/(2.*m).*t).*sin(sqrt((4.*m.*k-c.*c)./(4.*m.^2)).*t));
+w = sqrt(-(c/(2*m))^2 + (k/m));
+a = c/(2*m);
+u = 1./(w*m).*(1 - cos(w.*t).*exp(-a.*t) - a./w.*sin(w.*t).*exp(-a.*t))./(w + ((a.^2)./w));
 
 
 c = 1000;
-v = (1./(sqrt((4.*m.*k-c.*c)./4)).*exp(-c/(2.*m).*t).*sin(sqrt((4.*m.*k-c.*c)./(4.*m.^2)).*t));
+w = sqrt(-(c/(2*m))^2 + (k/m));
+a = c/(2*m);
+v = 1./(w*m).*(1 - cos(w.*t).*exp(-a.*t) - a./w.*sin(w.*t).*exp(-a.*t))./(w + ((a.^2)./w));
 plot(t,y,t,u,t,v);
 
 
