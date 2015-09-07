200f14249a035b8a02f1ddaa46cfca6f6695189b
Villpower <v.rydfalk@gmail.com>
2015-09-07 15:45:59 +0200

    Fix av bilder

--- FleraStegsvar.m
+++ FleraStegsvar.m
@@ -2,33 +2,23 @@ function FleraStegsvar( )
 %FLERAIMPULSSVAR Summary of this function goes here
 %   Detailed explanation goes here
 %
-%y = 1./(w*m).*(1 - cos(w.*t).*exp(-a.*t) -
-%a./w.*sin(w.*t).*exp(-a.*t))./(w + ((a.^2)./w)); FEL!!!
 
 k = 2000;
 c = 100;
 m = 70;
 
-w = sqrt(-(c/(2*m))^2 + (k/m));
-a = c/(2*m);
-
 t = linspace(0,10,1000);
 
-y = (1./(w.^2 + a.^2)) * (1 - cos(w.*t).*exp(-a.*t) - (a./w^2).*sin(w*t).*exp(-a.*t));
-
+y = (1./(sqrt((4.*m.*k-c.*c)./4)).*exp(-c/(2.*m).*t).*sin(sqrt((4.*m.*k-c.*c)./(4.*m.^2)).*t));
 hFig = figure(1);
 set(hFig, 'Position', [0,0,700,400]);
 
 c = 10;
-w = sqrt(-(c/(2*m))^2 + (k/m));
-a = c/(2*m);
-u = (1./(w.^2 + a.^2)) * (1 - cos(w.*t).*exp(-a.*t) - (a./w^2).*sin(w*t).*exp(-a.*t));
+u = (1./(sqrt((4.*m.*k-c.*c)./4)).*exp(-c/(2.*m).*t).*sin(sqrt((4.*m.*k-c.*c)./(4.*m.^2)).*t));
 
 
 c = 1000;
-w = sqrt(-(c/(2*m))^2 + (k/m));
-a = c/(2*m);
-v = (1./(w.^2 + a.^2)) * (1 - cos(w.*t).*exp(-a.*t) - (a./w^2).*sin(w*t).*exp(-a.*t));
+v = (1./(sqrt((4.*m.*k-c.*c)./4)).*exp(-c/(2.*m).*t).*sin(sqrt((4.*m.*k-c.*c)./(4.*m.^2)).*t));
 plot(t,y,t,u,t,v);
 
 
